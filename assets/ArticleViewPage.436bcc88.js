d8ece9c03aaf5c7db7f5c46ad3fece2472bd4780
iori2333 <81511507+iori2333@users.noreply.github.com>
2022-02-27 19:39:41 +0000

    Deploying to preview from @ iori2333/blog@dcad1fcc70d0c3f72280e7757b96b61d10d7e7c5 🚀

--- assets/ArticleViewPage.436bcc88.js
+++ assets/ArticleViewPage.436bcc88.js
@@ -1,4 +1,4 @@
-import{d as t,u as e,o as F,c as o,g as C,F as r}from"./vendor.c90ce3ea.js";import{A as a}from"./ArticleDetail.77da7f5a.js";import{P as i}from"./PageHeader.5a74ae1a.js";import"./LinkItem.2c17abdd.js";import"./index.5d9f55e4.js";const _=t({setup(s){const u={id:e().params.id,title:"\u535A\u5BA2\u6FC0\u60C5\u7F16\u5199\u4E2D",timestamp:new Date().getTime(),content:`
+import{d as t,u as e,o as F,c as o,g as C,F as r}from"./vendor.c90ce3ea.js";import{A as a}from"./ArticleDetail.eeb958be.js";import{P as i}from"./PageHeader.8bc94c3c.js";import"./LinkItem.e7e214c3.js";import"./index.77041061.js";const _=t({setup(s){const u={id:e().params.id,title:"\u535A\u5BA2\u6FC0\u60C5\u7F16\u5199\u4E2D",timestamp:new Date().getTime(),content:`
 # \u6807\u9898
 > \u54FC\u54FC\u54FC\uFF0C\u554A\u554A\u554A\u554A\u554A\u554A\u554A\u554A\u554A\u554A\u554A\u554A
 
---
+++ assets/MyPage.3047e37f.js
@@ -0,0 +1,9 @@
+import{A as e}from"./ArticleDetail.eeb958be.js";import{d as o,o as r,a}from"./vendor.c90ce3ea.js";import"./LinkItem.e7e214c3.js";import"./index.77041061.js";const A=o({setup(i){const u=`
+# Iori
+
+![me](https://avatars.githubusercontent.com/u/81511507?v=4)
+
+> \u96FB\u5B50\u306E\u6D77\u3092\u6F02\u3046\u30AA\u30BF\u30AF
+
+\u5357\u4EAC\u5927\u5B662019\u7EA7\u901A\u4FE1\u5DE5\u7A0B\u672C\u79D1\u751F\uFF0C\u5E9F\u7269\u662F\u4E5F\u3002
+`.trim(),t={id:"-1",title:"\u5173\u4E8E\u6211",content:u,author:"Iori",timestamp:new Date(2022,2,28,3,8).getTime()};return(c,n)=>(r(),a(e,{article:t}))}});export{A as default};
