559ed01fdfc62b11066b1f6cf08b321a23a7a48b
Iori Ichinose <milk.ichinose.iori@gmail.com>
2022-02-28 16:33:45 +0800

    fix: 3-digit colors

--- src/util/color.ts
+++ src/util/color.ts
@@ -4,12 +4,20 @@ export const textColor = (color: string) => {
   if (!colorReg.test(color)) {
     return '#000';
   }
-  color = color.substring(1);
-  const [r, g, b] = [
-    parseInt(color.substring(0, 2), 16),
-    parseInt(color.substring(2, 4), 16),
-    parseInt(color.substring(4, 6), 16)
-  ];
+  let [r, g, b] = [0, 0, 0];
+  if (color.length === 7) {
+    [r, g, b] = [
+      parseInt(color.substring(1, 3), 16),
+      parseInt(color.substring(3, 5), 16),
+      parseInt(color.substring(5, 7), 16)
+    ];
+  } else {
+    [r, g, b] = [
+      parseInt(color[1] + color[1], 16),
+      parseInt(color[2] + color[2], 16),
+      parseInt(color[3] + color[3], 16)
+    ];
+  }
   const thre = 0.213 * r + 0.715 * g + 0.072 * b;
   return thre > 255 / 2 ? '#000' : '#fff';
 };
