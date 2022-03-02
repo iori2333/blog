2f7d0af26ed60f00f9f59c0394b2a628a26521ac
iori2333 <81511507+iori2333@users.noreply.github.com>
2022-03-02 11:19:01 +0000

    Deploying to preview from @ iori2333/blog@ac583a763525216b08cedeab108c3009baaa7d0e 🚀

--- assets/MyPage.ddf37577.js
+++
@@ -1,9 +0,0 @@
-import{A as e}from"./ArticleDetail.bc4a83af.js";import{d as o,o as r,k as a}from"./vendor.526aba24.js";import"./LinkItem.f99ab3b7.js";import"./index.3910064e.js";const A=o({setup(i){const u=`
-# Iori
-
-![me](https://avatars.githubusercontent.com/u/81511507?v=4)
-
-> \u96FB\u5B50\u306E\u6D77\u3092\u6F02\u3046\u30AA\u30BF\u30AF
-
-\u5357\u4EAC\u5927\u5B662019\u7EA7\u901A\u4FE1\u5DE5\u7A0B\u672C\u79D1\u751F\uFF0C\u5E9F\u7269\u662F\u4E5F\u3002
-`.trim(),t={id:"-1",title:"\u5173\u4E8E\u6211",content:u,author:"Iori",timestamp:new Date(2022,2,28,3,8).getTime()};return(c,n)=>(r(),a(e,{article:t}))}});export{A as default};
